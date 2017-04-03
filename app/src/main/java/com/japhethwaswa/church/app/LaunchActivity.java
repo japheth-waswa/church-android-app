caa1d40cae0b7c9070ec936fa78fb3eed03b74e6
jefflilcot <japhethwaswa@gmail.com>
2017-04-03 15:02:45 +0300

    added splash screen successfuly

--- app/src/main/java/com/japhethwaswa/church/app/LaunchActivity.java
+++ app/src/main/java/com/japhethwaswa/church/app/LaunchActivity.java
@@ -1,4 +1,4 @@
-package com.japhethwaswa.church;
+package com.japhethwaswa.church.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        //todo start home activity
     }
 }
