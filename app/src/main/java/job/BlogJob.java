8265f415cdc202ef009f1faa715316358e7f0ee0
jefflilcot <japhethwaswa@gmail.com>
2017-05-15 14:10:00 +0300

    Implemented job to get service from remote

---
+++ app/src/main/java/job/BlogJob.java
@@ -0,0 +1,48 @@
+package job;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.birbit.android.jobqueue.Job;
+import com.birbit.android.jobqueue.Params;
+import com.birbit.android.jobqueue.RetryConstraint;
+
+import service.ChurchWebService;
+
+public class BlogJob extends Job{
+    public static final int PRIORITY = 1;
+
+    public BlogJob() {
+        // This job requires network connectivity,
+        // and should be persisted in case the application exits before job is completed.
+        super(new Params(PRIORITY).requireNetwork().persist());
+    }
+    @Override
+    public void onAdded() {
+        // Job has been saved to disk.
+        // This is a good place to dispatch a UI event to indicate the job will eventually run.
+        // In this example, it would be good to update the UI with the newly posted tweet.
+    }
+
+    @Override
+    public void onRun() throws Throwable {
+        // Job logic goes here. In this example, the network call to post to Twitter is done here.
+        // All work done here should be synchronous, a job is removed from the queue once
+        // onRun() finishes.
+        ChurchWebService.serviceGetBlog(getApplicationContext());
+    }
+
+    @Override
+    protected void onCancel(int cancelReason, @Nullable Throwable throwable) {
+        // Job has exceeded retry attempts or shouldReRunOnThrowable() has decided to cancel.
+    }
+
+    @Override
+    protected RetryConstraint shouldReRunOnThrowable(@NonNull Throwable throwable, int runCount, int maxRunCount) {
+        // An error occurred in onRun.
+        // Return value determines whether this job should retry or cancel. You can further
+        // specify a backoff strategy or change the job's priority. You can also apply the
+        // delay to the whole group to preserve jobs' running order.
+        return RetryConstraint.createExponentialBackoff(runCount, 1000);
+    }
+}
--- app/src/main/java/service/ChurchWebService.java
+++ app/src/main/java/service/ChurchWebService.java
@@ -30,6 +30,7 @@ import db.ChurchContract;
 import db.ChurchQueryHandler;
 import db.DatabaseHelper;
 import event.pojo.BibleUpdate;
+import event.pojo.BlogDataRetrievedSaved;
 import event.pojo.DonationDataRetrievedSaved;
 import event.pojo.EventDataRetrievedSaved;
 import event.pojo.ScheduleDataRetrievedSaved;
@@ -353,6 +354,158 @@ public class ChurchWebService {
 
         //post event
         EventBus.getDefault().post(new DonationDataRetrievedSaved());
+
+    }
+
+
+
+    public static void serviceGetBlog(final Context applicationContext) {
+
+        Resources res = applicationContext.getResources();
+        String relativeUrl = res.getString(R.string.app_blog);
+        String accessToken = OAuth.getClientCredentialsGrantTypeAccessToken();
+
+        AndroidNetworking.get(getAbsoluteUrl(applicationContext, relativeUrl))
+                .setPriority(Priority.HIGH)
+                .setTag("scheduleApi")
+                .addHeaders("Authorization", "Bearer " + accessToken)
+                .build().getAsJSONObject(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                parseBlogs(response, applicationContext);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+            }
+        });
+
+    }
+
+    private static void parseBlogs(JSONObject custJObject, Context applicationContext) {
+
+        JSONObject jObject = custJObject;
+        JSONArray blogCategoriesJArray = null;
+        JSONArray blogsJArray = null;
+
+        ChurchQueryHandler handler = new ChurchQueryHandler(applicationContext.getContentResolver());
+        handler.startDelete(5, null, ChurchContract.BlogCategoryEntry.CONTENT_URI, null, null);
+        handler.startDelete(7, null, ChurchContract.BlogsEntry.CONTENT_URI, null, null);
+        handler.startDelete(9, null, ChurchContract.BlogCommentsEntry.CONTENT_URI, null, null);
+
+
+
+        try {
+            //get jsonArray
+            blogCategoriesJArray = jObject.getJSONArray("blogCategories");
+
+
+            if (blogCategoriesJArray.length() > 0) {
+
+                for (int i = 0; i < blogCategoriesJArray.length(); i++) {
+
+                    try {
+
+                        JSONObject jObj = blogCategoriesJArray.getJSONObject(i);
+                        ContentValues valuesed = new ContentValues();
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_BLOG_CATEGORY_ID, jObj.getString("id"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN__TITLE, jObj.getString("title"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_URL_KEY, jObj.getString("url_key"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_DESCRIPTION, jObj.getString("description"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_VISIBLE, jObj.getString("visible"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_CREATED_AT, jObj.getString("created_at"));
+                        valuesed.put(ChurchContract.BlogCategoryEntry.COLUMN_UPDATED_AT, jObj.getString("updated_at"));
+
+                        handler.startInsert(21, null, ChurchContract.BlogCategoryEntry.CONTENT_URI, valuesed);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+            //get jsonArray
+            blogsJArray = jObject.getJSONArray("blogs");
+
+
+            if (blogsJArray.length() > 0) {
+
+                for (int i = 0; i < blogsJArray.length(); i++) {
+
+                    try {
+
+                        JSONObject jObj = blogsJArray.getJSONObject(i);
+                        ContentValues values = new ContentValues();
+                        values.put(ChurchContract.BlogsEntry.COLUMN_BLOG_ID, jObj.getString("id"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_TITLE, jObj.getString("title"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_URL_KEY, jObj.getString("url_key"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_IMAGE_URL, jObj.getString("image_url"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_BRIEF_DESCRIPTION, jObj.getString("brief_description"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_CONTENT, jObj.getString("content"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_AUTHOR_NAME, jObj.getString("author_name"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_PUBLISH_DATE, jObj.getString("publish_date"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_BLOG_CATEGORY_ID, jObj.getString("blog_category_id"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_VISIBLE, jObj.getString("visible"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_CREATED_AT, jObj.getString("created_at"));
+                        values.put(ChurchContract.BlogsEntry.COLUMN_UPDATED_AT, jObj.getString("updated_at"));
+                        handler.startInsert(23, null, ChurchContract.BlogsEntry.CONTENT_URI, values);
+
+                        //start insertion of comments if available
+                        parseBlogComments(applicationContext,jObj.getJSONArray("comments"));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        //post event for all subscribers that data has been received and saved in local database
+        EventBus.getDefault().post(new BlogDataRetrievedSaved());
+
+    }
+
+    private static void parseBlogComments(Context applicationContext, JSONArray comments) {
+
+        JSONArray commentsJArray = comments;
+
+        ChurchQueryHandler handler = new ChurchQueryHandler(applicationContext.getContentResolver());
+
+
+        if (commentsJArray.length() > 0) {
+
+            for (int i = 0; i < commentsJArray.length(); i++) {
+
+                try {
+
+                    JSONObject jObj = commentsJArray.getJSONObject(i);
+                    ContentValues valuesed = new ContentValues();
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_NAMES, jObj.getString("names"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_EMAIL, jObj.getString("email"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_PHONE, jObj.getString("phone"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_MESSAGE, jObj.getString("message"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_BLOG_ID, jObj.getString("blog_id"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_VISIBLE, jObj.getString("visible"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_CREATED_AT, jObj.getString("created_at"));
+                    valuesed.put(ChurchContract.BlogCommentsEntry.COLUMN_UPDATED_AT, jObj.getString("updated_at"));
+
+                    handler.startInsert(21, null, ChurchContract.BlogCommentsEntry.CONTENT_URI, valuesed);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
 
     }
 
